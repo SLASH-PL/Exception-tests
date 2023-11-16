d251908acc8b0e73218a73f4d2944a75fb03071b
Hubert Haładyna <hubert.haladyna@microsoft.wsei.edu.pl>
2023-11-16 18:25:06 +0100

    commit

--- exceptions 2/Program.cs
+++ exceptions 2/Program.cs
@@ -1,1 +1,25 @@
 ﻿
+try
+{
+    int a = int.Parse(Console.ReadLine());
+    int b = int.Parse(Console.ReadLine());
+    int c = int.Parse(Console.ReadLine());
+    int x = a * b * c;
+    Console.WriteLine(x);
+}
+catch (FormatException)
+{
+    Console.WriteLine("format exception, exit");
+}
+catch (ArgumentException)
+{
+    Console.WriteLine("argument exception, exit");
+}
+catch (OverflowException)
+{
+    Console.WriteLine("overflow exception, exit");
+}
+catch
+{
+    Console.WriteLine("non supported exception, exit");
+}
