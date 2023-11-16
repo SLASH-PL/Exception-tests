942d1f3a3ac8bdb467c14e95757f6ce17cd301fd
hubert.haladyna <hubert.haladyna@BR-17>
2023-11-16 18:08:26 +0100

    Add project files.

---
+++ Exception tests/Program.cs
@@ -0,0 +1,32 @@
+﻿//start
+Console.WriteLine(TrianglePerimeter(a: -1,b: -3,c: 3,precision: 4));
+
+
+
+
+/// <summary>
+/// Oblicza obwód trójkąta dowolnego dla zadanych długości boków, zaokrąglając wynik do podanej liczby cyfr po przecinku
+/// </summary>
+/// <param name="a">długość pierwszego boku, liczba całkowita nieujemna</param>
+/// <param name="b">długość drugiego boku, liczba całkowita nieujemna</param>
+/// <param name="c">długość trzeciego boku, liczba całkowita nieujemna</param>
+/// <param name="precision">dokładność obliczeń (zaokrąglenie), liczba cyfr po przecinku (od 2 do 8)</param>
+/// <returns>obwód trójkąta obliczony z zadaną dokładnością</returns>
+/// <exception cref="ArgumentOutOfRangeException">z komunikatem "wrong arguments", 
+///     gdy <c>precision</c> jest poza przedziałem od 2 do 8 lub którakolwiek z długości jest ujemna</exception>    
+/// <exception cref="ArgumentException">z komunikatem "object not exist", gdy trójkąta nie można utworzyć</exception>
+/// <remarks>dopuszczamy trójkąt o pokrywających się bokach lub o wszystkich bokach o długości 0</remarks>
+static double TrianglePerimeter(int a, int b, int c, int precision = 2)
+{
+    if (a < 0 || b < 0 || c < 0)
+        throw new ArgumentOutOfRangeException("wrong arguments");    
+
+    if (a + b < c || a + c < b || b + c < a)
+        throw new ArgumentException("object not exist");
+    
+    if (precision < 2 || precision > 8)
+        throw new ArgumentOutOfRangeException("wrong arguments");
+
+
+    return a + b + c;
+}
